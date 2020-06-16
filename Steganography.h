4c41ec93931509435b0a39ec044ebef633ec42b4
Nikita Vlasov <vlasow.nikita2016@yandex.ru>
2020-06-16 16:52:19 +0300

    first commit

--- Steganography.h
+++ Steganography.h
@@ -1,6 +1,22 @@
+/** 
+* @file Steganography.h
+* @author Власов Н.С.
+* @version 1.0
+* @date 15.20.2020
+* @copyright ИБСТ ПГУ
+* @brief Заголовочный файл для модуля Steganography
+*/
 #pragma once
 #include <string>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <fstream>
+#include <codecvt>
 using namespace std;
+/** 
+* @brief Программа для стеганографического преобрзования
+*/
 class Steganography {
     
 private:
@@ -8,6 +24,18 @@ private:
     string file_message;
     string file_new;
 public:
-    int extraction(string file_container);
     int concealment(string file_container, string file_message);
-    };
+    int extraction(string file_container);
+};
+
+/** @brief Класс обработки ошибок error_proj
+* @details Выводит сообщения об ошибках
+*/
+class error_proj: public std::invalid_argument
+{
+public:
+	explicit error_proj (const std::string& what_arg):
+		std::invalid_argument(what_arg) {}
+	explicit error_proj (const char* what_arg):
+		std::invalid_argument(what_arg) {}
+};
--- main.cpp
+++ main.cpp
@@ -1,38 +1,46 @@
-#include <iostream>
-#include <string>
+/** 
+* @file main.cpp
+* @brief Главный модуль программы для стеганографического преобрзования.
+*/
 #include "Steganography.h"
+
 int main()
 {
     setlocale(LC_ALL, "ru");
     
-    int key = 2;
-    
-    Steganography encryption;
-    
-    string container = "";
-    string messag = "";
-    
-    cout << "Если вы хотите скрыть сообщение, введите 1" << endl;
-    cout << "Если вы хотите извлечь сообщение, введите 0" << endl;
-    
-    cin >> key;
-        
-    if (key == 1)
-    {
-        cout << "Введите файл-контейнер: " << endl;
-        cin >> container;
-        
-        
-        cout << "Введите файл-сообщение: " << endl;
-        cin >> messag;
-        encryption.concealment(container, messag);
-        }
-        
-    else
-    {
-        cout << "Введите файл-контейнер: " << endl;
-        cin >> container;
-        encryption.extraction(container);
-        }
+	try{
+		int key = 2;
+		
+		Steganography encryption;
+		
+		string container = "";
+		string messag = "";
+		
+		cout << "Если вы хотите скрыть сообщение, введите 1" << endl;
+		cout << "Если вы хотите извлечь сообщение, введите 0" << endl;
+		
+		cin >> key;
+			
+		if (key == 1)
+		{
+			cout << "Введите файл-контейнер: " << endl;
+			cin >> container;
+			
+			cout << "Введите файл-сообщение: " << endl;
+			cin >> messag;
+			encryption.concealment(container, messag);
+			}
+			
+		else
+		{
+			cout << "Введите файл-контейнер: " << endl;
+			cin >> container;
+			encryption.extraction(container);
+		}
+	}
+	catch (const error_proj & e) {
+		std::cerr << e.what() << std::endl;
+	}
+	
     return 0;
-    }
+}
